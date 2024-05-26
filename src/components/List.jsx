4141418a7e19a22d8edc894547948f267141bc6b
Bharat Bisht <kamalbisht819@gmail.com>
2024-05-27 00:46:24 +0530

    second commit

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,12 +1,23 @@
-// import React from "react";
+import React, { useEffect } from "react";
 import PropTypes from "prop-types";
 
 const List = ({ name, age, button, handleEvent, disabled }) => {
+  useEffect(() => {
+    console.log(disabled);
+  }, []);
   return (
     <div className="flex gap-5 w-full justify-between mb-3 items-center">
       <p className="w-6/12 text-left">{name}</p>
       <p className="w-2/12">{age}</p>
-      <button className="w-4/12" onClick={handleEvent} disabled={disabled}>
+      <button
+        className={`w-4/12 ${
+          disabled
+            ? "bg-gray-400 cursor-not-allowed"
+            : "bg-blue-500 hover:bg-blue-700"
+        } text-white font-bold py-2 px-4 rounded`}
+        onClick={handleEvent}
+        disabled={disabled}
+      >
         {button}
       </button>
     </div>
@@ -17,7 +28,7 @@ List.propTypes = {
   age: PropTypes.number,
   button: PropTypes.string,
   handleEvent: PropTypes.func,
-  disabled: PropTypes.bool.isRequired,
+  disabled: PropTypes.bool,
 };
 
 export default List;
--- src/components/Team.jsx
+++ src/components/Team.jsx
@@ -16,28 +16,32 @@ const Team = () => {
   const avg = total / sortedCart.length;
 
   return (
-    <div className="border-[5px] p-5 max-h-[500px] w-[350px] box-border overflow-y-scroll flex flex-col gap-5 no-scrollbar justify-between">
-      <h1 className="font-bold text-4xl">Teams</h1>
-      {state.cart.length == 0 ? (
-        <p className="my-auto">There is no item inside the cart</p>
-      ) : (
-        <div>
-          {sortedCart.map((data) => (
-            <div key={data.id}>
-              <List
-                key={data.id}
-                name={data.first_name}
-                age={data.age}
-                button="Remove"
-                handleEvent={() => removeTeam(data)}
-              />
-            </div>
-          ))}
-        </div>
-      )}
-      <div>
-        <p>
-          Average age <span>{avg.toFixed(2)}</span>
+    <div className="border-[5px] p-5 h-[500px] w-[350px] box-border overflow-y-scroll flex flex-col gap-5 no-scrollbar">
+      <h1 className="font-bold text-4xl relative">
+        Teams
+        <span className="text-sm absolute right-2 top-[10px] bg-white rounded-xl text-black px-2">
+          sorted
+        </span>
+      </h1>
+      <div className="flex-1 overflow-y-scroll no-scrollbar">
+        {state.cart.length === 0 ? (
+          <p className="mt-[150px]">There is no item inside the cart</p>
+        ) : (
+          sortedCart.map((data) => (
+            <List
+              key={data.id}
+              name={data.first_name}
+              age={data.age}
+              button="Remove"
+              handleEvent={() => removeTeam(data)}
+              disabled={false}
+            />
+          ))
+        )}
+      </div>
+      <div className="sticky bottom-0 bg-white text-black py-2 border-t-2">
+        <p className="font-bold">
+          Average age: <span>{avg.toFixed(2)}</span>
         </p>
       </div>
     </div>
